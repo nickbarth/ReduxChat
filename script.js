cfad8976d3e63d3c51d827d38565a1d1cadf207b
nickbarth <hello@nickbarth.ca>
2018-02-18 17:12:58 -0700

    added assets

---
+++ script.js
@@ -0,0 +1,9 @@
+const { connect, Provider } = ReactRedux;
+const { createStore } = Redux;
+const { Component } = React;
+
+initialState = { }
+
+function reducer (state = initialState, action) {
+  return state;
+}
